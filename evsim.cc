9d726be2e7367f786a89253771ca1bdfb1d6077f
Einar Hov <einh@stud.ntnu.no>
2018-02-13 11:12:42 +0100

    Refactor selection code

--- evsim.cc
+++ evsim.cc
@@ -21,27 +21,6 @@ configuration conf;
 std::vector<std::unique_ptr<environmental_entity>> environmental_objects;
 std::vector<environmental_entity*> to_be_destroyed;
 
- class ClickQueryCallback : public b2QueryCallback {
- public:
-	std::optional<const b2Fixture*> found_fixture;
-
-	bool ReportFixture(b2Fixture* fixture) {
-		 switch(*static_cast<fixture_type*>(fixture->GetUserData())) {
-			case fixture_type::torso:
-				found_fixture = fixture;
-				return false;
-			case fixture_type::torso_predator:
-				found_fixture = fixture;
-				return false;
-			case fixture_type::yell:
-				std::cout << "Found yell" << std::endl;
-				return false;
-			default:
-				return true;
-		}
-	}
- };
-
 int evsim(int argc, char **argv) {
 	if(!glfwInit()) {
 		return -1;
@@ -114,28 +93,43 @@ int evsim(int argc, char **argv) {
 			conf.draw_yell = !conf.draw_yell;
 		}
 	});
+
 	glfwSetMouseButtonCallback(window, [] (GLFWwindow* window, int button, int action, int mods) {
-		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
 			double pos_x, pos_y;
 			glfwGetCursorPos(window, &pos_x, &pos_y);
-			glm::vec4 transformed {
-				-1 + 2.0*(pos_x)/(1024),
-				1 - 2.0*(pos_y)/(768),
-				0,
-				1
+
+			const auto transformed =
+				glm::inverse(projection) *
+				glm::vec4 { -1 + 2.0*(pos_x)/(1024), 1 - 2.0*(pos_y)/(768), 0, 1 }
+			;
+
+			struct : public b2QueryCallback {
+				std::optional<const b2Fixture*> found_fixture;
+				bool ReportFixture(b2Fixture* fixture) {
+					switch(*static_cast<fixture_type*>(fixture->GetUserData())) {
+						case fixture_type::torso:
+							found_fixture = fixture;
+							return false;
+						case fixture_type::torso_predator:
+							found_fixture = fixture;
+							return false;
+						default:
+							return true;
+					}
+				}
+			} cb;
+
+			const b2AABB bounding_box {
+				b2Vec2 { transformed.x - 0.01f, transformed.y - 0.01f },
+				b2Vec2 { transformed.x + 0.01f, transformed.y + 0.01f }
 			};
-			transformed = glm::inverse(projection) * transformed;
-			ClickQueryCallback queryCallback;
-			b2Vec2 lower(transformed.x-0.01, transformed.y-0.01);
-			b2Vec2 upper(transformed.x+0.01, transformed.y+0.01);
-			b2AABB aabb;
-			aabb.lowerBound = lower;
-			aabb.upperBound = upper;
-			world.QueryAABB( &queryCallback, aabb);
-			if(queryCallback.found_fixture) {
-				const auto *fixture = *queryCallback.found_fixture;
+
+			world.QueryAABB(&cb, bounding_box);
+			if(cb.found_fixture) {
+				const auto *fixture = *cb.found_fixture;
 				auto *agent = static_cast<entity*>(fixture->GetBody()->GetUserData());
-				agent->message(std::make_any<msg_plot>(msg_plot {}));
+				agent->message(std::make_any<msg_plot>());
 				const b2Vec2 pos = fixture->GetBody()->GetPosition();
 				printf("Mouse pos : %f, %f : \n", transformed.x,transformed.y);
 				printf("Object found at : %f, %f\n", pos.x, pos.y);
