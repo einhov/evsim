47ca8b06a237cbf4d353c4823beeb951a9aaafd1
Andreas H Weisethaunet <weisethaunet@hotmail.com>
2018-02-26 11:58:59 +0100

    Set compat_threshold in config.h to NEAT standard value

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ namespace evsim {
 namespace build_config {
 
 constexpr size_t food_count = 150;
-constexpr size_t herbivore_count = 50;
+constexpr size_t herbivore_count = 150;
 constexpr size_t predator_count = 50;
 
 // Physics
@@ -17,7 +17,7 @@ constexpr float angular_damping = 10.0f;
 // Hervivores
 constexpr size_t hv_min_species = 3;
 constexpr size_t hv_max_species = 20;
-constexpr double hv_compat_treshold = 0.1;
+constexpr double hv_compat_treshold = 5.0;
 constexpr float hv_force = 1000.0f;
 constexpr float hv_torque = 45.0f;
 
