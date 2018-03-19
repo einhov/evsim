5c5fe39629b6e059976642a7fcffe93f2097ca50
Andreas H Weisethaunet <weisethaunet@hotmail.com>
2018-03-19 14:12:40 +0100

    Add centered moving average plot and button to toggle gui auto range

--- ui/fitness_graph.cc
+++ ui/fitness_graph.cc
@@ -1,3 +1,5 @@
+#include <boost/circular_buffer.hpp>
+
 #include "fitness_graph.h"
 #include "ui_fitness_graph.h"
 
@@ -11,21 +13,49 @@ fitness_graph::fitness_graph(QWidget *parent) :
 	ui->setupUi(this);
 
 	auto chart_fitness = new QtCharts::QChart();
+	chart_fitness->setTheme(QtCharts::QChart::ChartThemeDark);
 
 	series_fitness = new QtCharts::QLineSeries();
-	chart_fitness->addSeries(series_fitness);
-
 	series_min_fitness = new QtCharts::QLineSeries();
-	chart_fitness->addSeries(series_min_fitness);
-
 	series_max_fitness = new QtCharts::QLineSeries();
-	chart_fitness->addSeries(series_max_fitness);
 
-	chart_fitness->setTheme(QtCharts::QChart::ChartThemeDark);
+
+	series_fitness_avg = new QtCharts::QLineSeries();
+	QPen pen;
+	pen.setStyle(Qt::DashLine);
+	pen.setColor("white");
+	series_fitness_avg->setPen(pen);
+	series_min_fitness_avg = new QtCharts::QLineSeries();
+	series_min_fitness_avg->setPen(pen);
+	series_max_fitness_avg = new QtCharts::QLineSeries();
+	series_max_fitness_avg->setPen(pen);
+
+	chart_fitness->addSeries(series_fitness);
+	chart_fitness->addSeries(series_min_fitness);
+	chart_fitness->addSeries(series_max_fitness);
+	chart_fitness->addSeries(series_fitness_avg);
+	chart_fitness->addSeries(series_min_fitness_avg);
+	chart_fitness->addSeries(series_max_fitness_avg);
+
+
 	chart_fitness->createDefaultAxes();
+	chart_fitness->legend()->hide();
 	ui->fitness->setChart(chart_fitness);
 	ui->fitness->setRenderHint(QPainter::Antialiasing);
 	ui->fitness->setRubberBand(QtCharts::QChartView::RectangleRubberBand);
+
+	auto_range = true;
+
+	score_window_max = 10;
+
+	score_count = 0;
+	score_total_avg = 0;
+	score_total_min = 0;
+	score_total_max = 0;
+
+	scores_avg.set_capacity(score_window_max*2+1);
+	scores_min.set_capacity(score_window_max*2+1);
+	scores_max.set_capacity(score_window_max*2+1);
 }
 
 fitness_graph::~fitness_graph()
@@ -37,11 +67,46 @@ void fitness_graph::insert_fitness(int epoch, double fitness, double minimum_fit
 	series_fitness->append(epoch, fitness);
 	series_min_fitness->append(epoch, minimum_fitness);
 	series_max_fitness->append(epoch, maximum_fitness);
+	append_avg(epoch, fitness, minimum_fitness, maximum_fitness);
 	const auto [min, max] = std::minmax({ fitness, minimum_fitness, maximum_fitness });
 	if(epoch < series_range.xmin) series_range.xmin = epoch;
 	if(min < series_range.ymin) series_range.ymin = min;
 	if(epoch > series_range.xmax) series_range.xmax = epoch;
 	if(max > series_range.ymax) series_range.ymax = max;
+	if(auto_range) {
+		ui->fitness->chart()->axisX()->setRange(series_range.xmin, series_range.xmax);
+		ui->fitness->chart()->axisY()->setRange(series_range.ymin, series_range.ymax);
+	}
+}
+
+void fitness_graph::on_auto_range_clicked(bool checked) {
 	ui->fitness->chart()->axisX()->setRange(series_range.xmin, series_range.xmax);
 	ui->fitness->chart()->axisY()->setRange(series_range.ymin, series_range.ymax);
+	auto_range = checked;
+}
+
+void fitness_graph::append_avg(double epoch, double fitness, double minimum_fitness, double maximum_fitness) {
+	score_count++;
+	//Remove oldest element from total scores if circular_list is full
+	if(score_count >= score_window_max*2+1) {
+		score_total_avg -= scores_avg[0];
+		score_total_min -= scores_min[0];
+		score_total_max -= scores_max[0];
+	}
+	//Add new elements to circular buffer
+	scores_avg.push_back(fitness);
+	scores_min.push_back(minimum_fitness);
+	scores_max.push_back(maximum_fitness);
+
+	//Add the new fitness to the totals
+	score_total_avg += fitness;
+	score_total_min += minimum_fitness;
+	score_total_max += maximum_fitness;
+
+	if(score_count >= score_window_max*2+1) {
+		//Set the new values
+		series_fitness_avg->append(epoch-score_window_max, score_total_avg / (score_window_max*2+1));
+		series_max_fitness_avg->append(epoch-score_window_max, score_total_min / (score_window_max*2+1));
+		series_min_fitness_avg->append(epoch-score_window_max, score_total_max / (score_window_max*2+1));
+	}
 }
--- ui/fitness_graph.h
+++ ui/fitness_graph.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QEvent>
 #include <QtCharts/QLineSeries>
+#include <boost/circular_buffer.hpp>
 
 #include "../species.h"
 
@@ -20,11 +21,29 @@ public:
 	void insert_fitness(int epoch, double fitness, double minimum_fitness, double maximum_fitness);
 	~fitness_graph();
 
+private slots:
+	void on_auto_range_clicked(bool checked);
+
 private:
+	bool auto_range;
+	unsigned int score_count;
+	unsigned int score_window_max;
+	double score_total_avg;
+	double score_total_min;
+	double score_total_max;
+	boost::circular_buffer<double> scores_avg;
+	boost::circular_buffer<double> scores_min;
+	boost::circular_buffer<double> scores_max;
+
 	Ui::fitness_graph *ui;
 	QtCharts::QLineSeries *series_fitness;
 	QtCharts::QLineSeries *series_min_fitness;
 	QtCharts::QLineSeries *series_max_fitness;
+	QtCharts::QLineSeries *series_fitness_avg;
+	QtCharts::QLineSeries *series_min_fitness_avg;
+	QtCharts::QLineSeries *series_max_fitness_avg;
+
+	void append_avg(double epoch, double fitness, double minimum_fitness, double maximum_fitness);
 
 	struct { double xmin, xmax, ymin, ymax; } series_range {
 		0.0, 1.0, 0.0, 1.0
