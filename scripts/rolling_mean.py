bda99296ae600bb323c6605273b3bf7c610c43e5
Einar Hov <einh@stud.ntnu.no>
2018-04-26 12:10:07 +0200

    Take column to average from arguments

--- scripts/rolling_mean.py
+++ scripts/rolling_mean.py
@@ -1,7 +1,8 @@
+import sys
 import pandas as pd
 
 with open("scores") as scores:
-    goal_counts = [score.split()[4] for score in scores]
+    goal_counts = [score.split()[int(sys.argv[1])] for score in scores]
     df = pd.DataFrame({'in_goal': goal_counts}).rolling(window=55, center=True).mean()
     for row in df.itertuples():
         print("{} {}".format(row[0], row[1]))
